71ef86b1a9265acf83d7572231e30cffa32e8d06
hammo92 <james_hammond@live.com>
2021-11-01 10:43:55 +0000

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "@definitions/chakra/theme";
+import { StyledThemeProvider } from "@definitions/styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    const queryClient = new QueryClient();
+    return (
+        <ChakraProvider theme={theme}>
+            <StyledThemeProvider>
+                <QueryClientProvider client={queryClient}>
+                    <Hydrate state={pageProps.dehydratedState}>
+                        <Component {...pageProps} />
+                    </Hydrate>
+                </QueryClientProvider>
+            </StyledThemeProvider>
+        </ChakraProvider>
+    );
+}
+
+export default MyApp;
