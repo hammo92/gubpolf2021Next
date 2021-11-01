c8bc81aa5a8924415bf68052884f66d886e6f962
hammo92 <james_hammond@live.com>
2021-11-01 13:53:01 +0000

    remove theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@definitions/chakra/theme";
-import { StyledThemeProvider } from "@definitions/styled-components";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
 
@@ -10,13 +9,11 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     const queryClient = new QueryClient();
     return (
         <ChakraProvider theme={theme}>
-            <StyledThemeProvider>
-                <QueryClientProvider client={queryClient}>
-                    <Hydrate state={pageProps.dehydratedState}>
-                        <Component {...pageProps} />
-                    </Hydrate>
-                </QueryClientProvider>
-            </StyledThemeProvider>
+            <QueryClientProvider client={queryClient}>
+                <Hydrate state={pageProps.dehydratedState}>
+                    <Component {...pageProps} />
+                </Hydrate>
+            </QueryClientProvider>
         </ChakraProvider>
     );
 }
